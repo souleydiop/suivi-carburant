f5ad951d07d8b96be7dbce36d36dcbea96aacf0b
souleydiop <44343615+souleydiop@users.noreply.github.com>
2026-05-24 21:25:40 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,37 @@
-const CACHE = 'fuel-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE = 'fuel-cache-v1';
+const ASSETS = [
+  '/suivi-carburant/',
+  '/suivi-carburant/index.html',
+  '/suivi-carburant/manifest.json',
+  '/suivi-carburant/icon-192.png',
+  '/suivi-carburant/icon-512.png',
+  '/suivi-carburant/screenshot1.png',
+  '/suivi-carburant/screenshot2.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks =>
-    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return resp;
+      }).catch(() => caches.match('/suivi-carburant/index.html'));
+    })
   );
 });
